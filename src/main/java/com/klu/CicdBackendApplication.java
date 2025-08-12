21fe6b3be78f08cf1580cc48931e34aa9492a902
2300030168 <2300030168@kluniversity.in>
2025-08-12 19:35:17 +0530

    updated pom file

--- src/main/java/com/klu/CicdBackendApplication.java
+++ src/main/java/com/klu/CicdBackendApplication.java
@@ -1,14 +1,20 @@
+
 package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdBackendApplication {
+public class CicdBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CicdBackendApplication.class, args);
-		System.out.println("Running");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CicdBackendApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(CicdBackendApplication.class, args);
+    }
 }
